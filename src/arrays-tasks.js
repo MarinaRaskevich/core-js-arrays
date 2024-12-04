cc643af0340d68b92fbfb7f1aa6a442ea96d0b12
MarinaRaskevich <marina.raskevich19@gmail.com>
2024-12-04 16:42:12 +0100

    feat: add solution for getIdentityMatrix()

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -409,7 +409,9 @@ function getFalsyValuesCount(arr) {
  *                              [0,0,0,0,1]]
  */
 function getIdentityMatrix(n) {
-  throw new Error('Not implemented');
+  return Array.from({ length: n }, (_, rowIndex) =>
+    Array.from({ length: n }, (__, colIndex) => (rowIndex === colIndex ? 1 : 0))
+  );
 }
 
 /**
